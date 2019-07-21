0ad12b0e001c1ba5b0a9fc40f107cf2342f66e67
Hugo Andrés Vázquez <hugo@nubeit.net>
2019-07-21 12:35:37 -0300

    Update larainstaller.sh

--- larainstaller.sh
+++ larainstaller.sh
@@ -89,11 +89,7 @@ if ! type "composer" > /dev/null; then
 	    case $yn in
 	        Yes ) 
 				echo -e "${GREEN}Installing Composer...${NC}"
-
-				php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-				php -r "if (hash_file('SHA384', 'composer-setup.php') === 'e115a8dc7871f15d853148a7fbac7da27d6c0030b848d9b3dc09e2a0388afed865e6a3d6b3c0fad45c48e2b5fc1196ae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-				php composer-setup.php
-				php -r "unlink('composer-setup.php');"
+				curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 				mv composer.phar /usr/local/bin/composer
 			break;;
 	        No ) 
